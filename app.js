5e46d90b8ae0159c7e59a1196e0259aab09fb9ba
Karan Jariwala <100619029@oncampus.local>
2018-11-20 16:57:12 -0500

    Cleaned up app.js

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
 var express = require('express');
 const PORT = 3000
 var flash = require('connect-flash');
-
 var passport = require("passport");
 var request = require('request');
-
 var session = require("express-session");
-
 var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+const expressSession = require('express-session');
+
 
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
-
-const expressSession = require('express-session');
 app.use(expressSession({secret: 'mySecretKey'}));
 app.use(passport.initialize());
 app.use(passport.session());
-
-var bodyParser = require('body-parser')
-
-var path = require('path');
-
-//app.use('/public', express.static(__dirname + '/public'));
 
 app.use(flash());
 app.use(session({secret: 'keyboard cat'}))
